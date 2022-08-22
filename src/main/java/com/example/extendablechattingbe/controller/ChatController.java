137fbdad1c0032008378a7be05339d69865084c8
jmjmjames <chungjm0711@gmail.com>
2022-08-23 01:36:54 +0900

    Feat: DB에 Json 형식의 저장을 확인
    ObjectMapper mapper = new ObjectMapper();
    String message = mapper.writeValueAsString(chatMessage);

--- src/main/java/com/example/extendablechattingbe/controller/ChatController.java
+++ src/main/java/com/example/extendablechattingbe/controller/ChatController.java
@@ -1,6 +1,8 @@
 package com.example.extendablechattingbe.controller;
 
 import com.example.extendablechattingbe.dto.ChatMessage;
+import com.example.extendablechattingbe.service.ChatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -8,7 +10,10 @@ import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
+
+    private final ChatService chatService;
 
     @MessageMapping("/chat.register")
     @SendTo("/topic/public")
@@ -20,6 +25,7 @@ public class ChatController {
     @MessageMapping("/chat.send")
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        chatService.save(chatMessage);
         return chatMessage;
     }
 }
