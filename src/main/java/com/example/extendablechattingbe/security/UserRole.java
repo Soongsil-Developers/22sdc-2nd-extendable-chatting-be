b95876d4a3af857146e1c48696de061ed61cd75b
jmjmjames <chungjm0711@gmail.com>
2022-08-29 23:32:54 +0900

    Feat: security 와 관련된 User 정보 (#6)
    UserPrincipal implements UserDetails 을 구현하여 SecurityContextHolder 에 담는다.

---
+++ src/main/java/com/example/extendablechattingbe/security/UserRole.java
@@ -0,0 +1,16 @@
+package com.example.extendablechattingbe.security;
+
+import lombok.Getter;
+
+public enum UserRole {
+    USER("ROLE_USER"), // ROLE_ : Spring Security 권한 표현을 하는 문자열 규칙
+    ADMIN("ROLE_ADMIN");
+
+    @Getter
+    private final String name;
+
+    UserRole(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/example/extendablechattingbe/user/UserService.java
@@ -0,0 +1,58 @@
+package com.example.extendablechattingbe.user;
+
+import com.example.extendablechattingbe.security.util.JwtTokenUtils;
+import com.example.extendablechattingbe.user.dto.UserDto;
+import com.example.extendablechattingbe.user.entity.User;
+import com.example.extendablechattingbe.user.exception.InvalidPasswordException;
+import com.example.extendablechattingbe.user.exception.UserNameDuplicatedException;
+import com.example.extendablechattingbe.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
+
+    @Value("${jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${jwt.token-expired-time-ms}")
+    private Long expiredTimeMs;
+
+    @Transactional(readOnly = true)
+    public UserDto loadUserByUserName(String userName) {
+        return userRepository.findByUserName(userName)
+                .map(UserDto::fromEntity)
+                .orElseThrow(() -> new UserNotFoundException("해당 유저를 찾을 수 없습니다."));
+    }
+
+    public UserDto join(UserDto dto) {
+        userRepository.findByUserName(dto.getUserName()).ifPresent(it -> {
+            throw new UserNameDuplicatedException("중복된 아이디입니다.");
+        });
+
+        User user = userRepository.save(User.of(dto.getUserName(), encoder.encode(dto.getPassword()), dto.getNickname()));
+
+        return UserDto.fromEntity(user);
+    }
+
+    @Transactional(readOnly = true)
+    public String login(UserDto dto) {
+        User user = userRepository.findByUserName(dto.getUserName())
+                .orElseThrow(() -> new UserNotFoundException("해당 유저를 찾을 수 없습니다."));
+
+        if (!encoder.matches(dto.getPassword(), user.getPassword())) {
+            throw new InvalidPasswordException("유효하지 않은 비밀번호입니다.");
+        }
+
+        return JwtTokenUtils.generateToken(dto.getUserName(), secretKey, expiredTimeMs);
+    }
+
+}
