e2a37414b3482aeef6b6553b7d76dac27b7fe2b1
jmjmjames <chungjm0711@gmail.com>
2022-09-15 20:35:33 +0900

    [Feat] GlobalControllerAdvice

--- src/main/java/com/example/extendablechattingbe/common/exception/GlobalControllerAdvice.java
+++ src/main/java/com/example/extendablechattingbe/common/exception/GlobalControllerAdvice.java
@@ -1,10 +1,11 @@
 package com.example.extendablechattingbe.common.exception;
 
 import com.example.extendablechattingbe.common.code.CommonCode;
+import com.example.extendablechattingbe.common.code.RoomCode;
+import com.example.extendablechattingbe.common.code.UserCode;
 import com.example.extendablechattingbe.common.response.Response;
-import com.example.extendablechattingbe.room.code.RoomCode;
-import com.example.extendablechattingbe.user.code.UserCode;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,14 +21,18 @@ public class GlobalControllerAdvice {
     }
 
     @ExceptionHandler(RoomNotFoundException.class)
-    public ResponseEntity<Response> handleNotFoundRoomException() {
+    public ResponseEntity<Response<Void>> handleNotFoundRoomException() {
         return ResponseEntity.ok().body(Response.of(RoomCode.ROOM_NOT_FOUND, null));
     }
 
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Response<Void>> handleNotFoundUserException() {
+        return ResponseEntity.ok().body(Response.of(UserCode.USER_NOT_FOUND, null));
+    }
+
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<Response> handlerRuntimeException(RuntimeException e) {
+    public ResponseEntity<Response<Void>> handlerRuntimeException(RuntimeException e) {
         log.error(e.getMessage());
         return ResponseEntity.ok(Response.of(CommonCode.INTERNAL_SERVER_ERROR, null));
     }
-
 }
